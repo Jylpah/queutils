a0a84e091fd597a6b4afe325742b58e030c98dd3
Jylpah <jylpah@gmail.com>
2025-02-23 22:59:16 +0200

    add CategoryCounterQueue

--- src/queutils/__init__.py
+++ src/queutils/__init__.py
@@ -2,10 +2,16 @@ from .countable import Countable as Countable
 from .asyncqueue import AsyncQueue as AsyncQueue
 from .iterablequeue import IterableQueue as IterableQueue, QueueDone as QueueDone
 from .filequeue import FileQueue as FileQueue
+from .counterqueue import (
+    CounterQueue as CounterQueue,
+    QCounter as QCounter,
+    CategoryCounterQueue as CategoryCounterQueue,
+)
 
 __all__ = [
     "asyncqueue",
     "countable",
+    "counterqueue",
     "filequeue",
     "iterablequeue",
 ]
